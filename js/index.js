926886ff90506dc7b51c1da15542a5829f866e4d
davi33-emanoel <emanoeldavi348@gmail.com>
2021-11-14 15:13:06 -0300

    Meu portifólio dos trabalhos feitos no Resilia

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
